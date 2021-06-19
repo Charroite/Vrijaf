92eec7f5b0edf5c9795b1928c298f380442e9f95
Mauriccio <mauricemr@outlook.com>
2021-06-19 12:09:16 +0200

    renamed ball audio script

---
+++ Vrijaf/Assets/Models/Activity/Tafeltennis/Scripts/BallAudioManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallAudioManager : MonoBehaviour
+{
+    public AudioSource hitDefault;
+    public AudioSource hitRacket;
+    public AudioClip hitDefaultSound;
+    public AudioClip hitRacketSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hitDefault = gameObject.AddComponent<AudioSource>();
+        hitRacket = gameObject.AddComponent<AudioSource>();
+
+        hitRacket.clip = hitRacketSound;
+        hitDefault.clip = hitDefaultSound;
+
+        hitDefault.playOnAwake = false;
+        hitRacket.playOnAwake = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Racket")
+        {
+            hitRacket.Play();
+        } 
+        else{
+            hitDefault.Play();
+        }
+    }
+}
